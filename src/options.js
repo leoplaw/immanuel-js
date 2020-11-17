3cd20b7e837b24542bf90525b7487d837930848c
Robert Davies <rob@sunlightdigital.net>
2020-11-16 18:43:05 -0800

    Initial commit.

---
+++ src/options.js
@@ -0,0 +1,38 @@
+export default class Options {
+
+    static defaults = {
+        rotateToHorizon: true,
+        rotateSigns: true,
+        rotateHouseNumbers: false,
+        rotateAngleText: false,
+        angleFormat: '%D&deg;%M\'',
+        lineOrder: [
+            'houses',
+            'angleMarkers',
+            'anglePointers',
+            'aspects',
+        ],
+        aspectOrder: [
+            'trine',
+            'sextile',
+            'semisextile',
+            'square',
+            'semisquare',
+            'sesquisquare',
+            'opposite',
+            'quintile',
+            'semiquintile',
+            'sesquiquintile',
+            'biquintile',
+            'quincunx'
+        ],
+    };
+
+    static create(options) {
+        return {
+            ...this.defaults,
+            ...options,
+        };
+    }
+
+}
